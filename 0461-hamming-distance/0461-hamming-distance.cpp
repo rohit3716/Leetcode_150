9c6edd31b4b7255a03e935ea617441a2ed410c1d
Rohit Raj <88617637+rohit3716@users.noreply.github.com>
2024-02-29 22:37:52 +0530

    Time: 0 ms (100%), Space: 7 MB (59.7%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans = 0;
+        while( x > 0 || y > 0 ){
+            if( ( x&1 ) != ( y&1 ) ) 
+                ans++;
+            // x >>= 1;
+            // y >>= 1;
+            //or
+            x /= 2;
+            y /= 2;
+        }
+        return ans;
+    }
+};
