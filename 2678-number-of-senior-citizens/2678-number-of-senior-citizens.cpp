3d6b3b5cd5f60c6f6892ea958fafb816e5ab2e10
Rohit Raj <88617637+rohit3716@users.noreply.github.com>
2024-08-02 00:28:14 +0530

    Time: 8 ms (47.01%), Space: 17.6 MB (27.35%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -3,13 +3,13 @@ public:
     int countSeniors(vector<string>& details) {
         int count = 0;
         for( auto str:details ){
-            int tens = str[11]-'0';
-            if( tens > 6 ){
+            int tens = str[11];
+            if( tens > '6' ){
                 count++;
             }
-            else if( tens == 6 ){
-                int ones = str[12]-'0';
-                if( ones > 0 ){
+            else if( tens == '6' ){
+                int ones = str[12];
+                if( ones > '0' ){
                     count++;
                 }
             } 
