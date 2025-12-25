c0b32034697428ee7c84ec43df363efe8a66c989
Rohit Raj <88617637+rohit3716@users.noreply.github.com>
2025-12-25 23:48:41 +0530

    Time: 43 ms (43.55%), Space: 108.9 MB (76.34%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -3,14 +3,14 @@ class Solution {
         int j = 0;
         long maxHappiness = 0;
         Arrays.sort(happiness);
-        
+
         for (int i = 0; i < happiness.length / 2; i++) {
             int temp = happiness[i];
             happiness[i] = happiness[happiness.length - 1 - i];
             happiness[happiness.length - 1 - i] = temp;
         }
         while (k > 0) {
-            maxHappiness = maxHappiness + Math.abs(happiness[j] - j);
+            maxHappiness = maxHappiness + ((happiness[j] - j) <= 0 ? 0 : (happiness[j] - j));
             k--;
             j++;
         }
